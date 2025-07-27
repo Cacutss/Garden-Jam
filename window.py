f72c9203ad5eca661223a56f705bc05fcd458483
Cacutss <cuentasmurfdeleague@gmail.com>
2025-07-26 22:40:37 -0300

    added constants

--- window.py
+++ window.py
@@ -6,15 +6,14 @@ import audio_extractor
 import threading
 import queue
 import export_video
+from constants import *
 
-WIN_WIDTH = 1920
-WIN_HEIGHT = 1080
-ZEROES = "00000000"
+AUDIO_FILE="./Test assets/DeepDive.ogg"
 
 class Bar:
     def __init__(self,x:int,y:int,width:int):
         self.rect = pygame.Rect((x,y),(width,0))
-        self.color = pygame.Color(255,255,255,30)
+        self.color = BAR_COLOR + (BAR_TRANSPARENCY,)
 
     def update(self,data):
         #max is half the screen so 255 / 255 * 540 = 540
@@ -50,16 +49,38 @@ class BarGroup:
         for i in range(len(self.bars)):
             self.bars[i].draw(screen)
 
-def draw_rect(rect,screen):
-    pygame.draw.rect(surface=screen,color=(255,255,255),rect=rect)
+def draw_rect(rect,screen,color):
+    pygame.draw.rect(surface=screen,color=color,rect=rect)
 
 class Window():
     def __init__(self,audio_path):
         self.screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT),pygame.SRCALPHA,pygame.HIDDEN)
         self.audio_path = audio_path
         self.audio_data = audio_extractor.AudioDataSet(audio_path)
+        self.bargroups = []  
+        self.car_cooldown = [0,0,0,0,0,0,0,0,0,0]
         self.frogboard = frogboard.Frogger_Board()
-        self.frogboard.divider_thickness = 2
+
+    def update(self,data):
+        self.frogboard.update()
+        for i in range(len(data)):
+            if self.car_cooldown[i] == 0:
+                if data[i] > 250:
+                    self.frogboard.generate_car(i)
+                    self.car_cooldown[i] = 30
+            else:
+                self.car_cooldown[i] -= 1
+        for i in range(len(self.bargroups)):
+            self.bargroups[i].update(data[i])
+
+    def draw(self):
+        for i in range(len(self.bargroups)):
+            self.bargroups[i].draw(self.screen)
+        cars = self.frogboard.get_all_car_rects()
+        frog = self.frogboard.get_frog_rect()
+        for car in cars:
+            draw_rect(car,self.screen,CAR_COLOR_LEFT)
+        draw_rect(frog,self.screen,FROG_COLOR)
 
     def run(self):
         pygame.init()
@@ -70,8 +91,6 @@ class Window():
         #totalframes is the number we iterate on to create each frame
         totalframes = self.audio_data.get_total_frames()
         frame_queue = queue.Queue(maxsize=50)
-        bargroups = []  
-        car_cooldown = [0,0,0,0,0,0,0,0,0,0]
         saving_thread = threading.Thread(target=save_frame_temp,args=(frame_queue,export_video.get_next_filename()))
         saving_thread.start()
         for i in range(0,10):
@@ -80,42 +99,28 @@ class Window():
             #gives all the bargroups the same width
             width = int(WIN_WIDTH/10)
             #gives all the bars their unique spacing
-            bargroups.append(BarGroup(amount=4,x = x,width=width,barwidth=int(width/4)))
+            self.bargroups.append(BarGroup(amount=4,x = x,width=width,barwidth=int(width/4)))
         for frame in range(totalframes):
+            print(f"{frame}/{totalframes}")
             self.screen.fill((0,0,0))
             data = self.audio_data.get_visual_ranges(frame_index=frame)
-            for i in range(len(data)):
-                if car_cooldown[i] == 0:
-                    if data[i] > 250:
-                        self.frogboard.generate_car(i)
-                        car_cooldown[i] = 30
-                else:
-                    car_cooldown[i] -= 1
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                    frame_queue.put(None)
                     return 0
             #updates everything
-            self.frogboard.update()
-            cars = self.frogboard.get_all_car_rects()
-            for i in range(len(bargroups)):
-                bargroups[i].update(data[i])
+            self.update(data)
             #draws everything
-            for i in range(len(bargroups)):
-                bargroups[i].draw(self.screen)
-            for car in cars:
-                try:
-                    draw_rect(car,self.screen)
-                except Exception as e:
-                    pass
+            self.draw()
             pygame.display.update()
             #saves screen to folder
             frame_queue.put(self.screen.copy())
             framecount += 1
+        pygame.quit()
         frame_queue.put(None)
         frame_queue.join()
         saving_thread.join()
-        pygame.quit()
 
 def save_frame_temp(queue,output_path):
     print("starting saving thread")
@@ -127,7 +132,7 @@ def save_frame_temp(queue,output_path):
             '-s', f"{WIN_WIDTH}x{WIN_HEIGHT}",
             '-r', "60",
             '-i', 'pipe:',
-            "-i", "./Test assets/cat.mp3",          # Path to your audio file (handle spaces with quotes or as a list element)
+            "-i", AUDIO_FILE,          # Path to your audio file (handle spaces with quotes or as a list element)
             '-c:v', 'libx264',
             '-preset', 'fast',
             '-crf', '23',
