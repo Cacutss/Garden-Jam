33e44d2ecdd4ed02cd697a4d6d2f76427164cc38
hermanvadym <vadymherman@gmail.com>
2025-07-27 13:46:44 +0200

    small chaganes in main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import export_video
 from welcome_screen import show_instruction_screen
 
 def main():
-    print("Hello from gardenjam!")
+    gamewindow = window.Window("Test assets/beeps.wav")
+    gamewindow.run()
     # Show the Pygame welcome window (with "Upload" button)
     audio_path = show_instruction_screen()
     if not audio_path:
@@ -12,8 +13,7 @@ def main():
     # create_window(audio_path)   # Launch visualization as needed
     export_video.merge_export(audio_path)
 
-    gamewindow = window.Window("Test assets/beeps.wav")
-    gamewindow.run()
+
 
 if __name__ == "__main__":
     main()
