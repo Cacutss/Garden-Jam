2c656192356b9735be48169376e6dfc809913014
Cacutss <cuentasmurfdeleague@gmail.com>
2025-07-27 12:29:16 -0300

    git bugging

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import window
 from welcome_screen import show_instruction_screen
 
 def main():
-   
     # Show the Pygame welcome window (with "Upload" button)
     audio_path = show_instruction_screen()
     if not audio_path:
